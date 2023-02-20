60ee187414555781b76eec7481c140a37e880dc4
branit490 <bs585@njit.edu>
2023-02-20 20:51:50 +0000

    this should all be in working order. registration html and php are updated with correct variable names

--- registration.php
+++ registration.php
@@ -16,17 +16,18 @@ $username = $storePost["username"];
 $password = $storePost["password"];
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 $request = array();
-$request['type'] = "validate";
+$request['type'] = "register";
 $request['username'] = $username;
 $request['password'] = $password;
 $request['message'] = "HI";
 $response = $client->send_request($request);
-print_r($response);
-if ($response['message'] == "Valid Login")
+//print_r($response);
+if ($response['message'] == "User Registered")
 {
-	$msg = "Login Successful";
+	$msg = "Registration Successful";
 } else {
-	$msg = "Invalid Login";
+	$msg = "User already exists! <a href='index.html'>Return to login page here</a>";
+	
 }
 echo json_encode($msg);
 exit(0);
