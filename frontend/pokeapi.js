f7362b16b3665f7f127f63114acc040d3865217a
branit490 <bs585@njit.edu>
2023-03-09 17:57:08 +0000

    saving for now

--- frontend/pokeapi.js
+++ frontend/pokeapi.js
@@ -1,12 +1,14 @@
 const form = document.querySelector('form');
 const resultsDiv = document.querySelector('#results');
+const resultsDiv2 = document.querySelector('#moves');
 
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
   
-  const pokemonName = document.querySelector('#pokemon').value;
+  const pokemonName = document.querySelector('#pokemon').value.toLowerCase();
+  const generation = document.querySelector('#generation').value;
   
-  const url = `https://pokeapi.co/api/v2/pokemon/${encodeURIComponent(pokemonName)}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
   const response = await fetch(url);
   
   if (!response.ok) {
@@ -15,11 +17,50 @@ form.addEventListener('submit', async (event) => {
   }
   
   const pokemon = await response.json();
+  //json to array ^^
   
-  const moveSet = pokemon.moves.map((move) => {
-    return move.move.name;
+  resultsDiv2.innerHTML = "";
+
+  var result = [];
+  
+  
+  for (var i in pokemon.moves) {
+
+ //   result.push([i, pokemon.moves[i]]);
+      move = i.move.name;
+      resultsDiv2.innerHTML += '<option value="1">'+move+'</option>';
+      //i.move.name;
+  }
+  
+  //resultsDiv2.innerHTML = "";
+  /*
+  for (var i in result){
+    
+    resultsDiv2.innerHTML += '<option value="1">Generation 1</option>';
+
+  }
+  */
+
+   
+  const generationIndex = generation - 1;
+  const moveSet = pokemon.moves.filter((move) => {
+    return move.version_group_details.some((versionGroupDetail) => {
+      return versionGroupDetail.version_group.url.includes(`/${generationIndex + 1}/`);
+      });
+    }).map((move) => {
+      return move.move.name;
   });
   
+ /* 
+  if (moveSet.length === 0 && pokemon.moves.length > 0) {
+    resultsDiv.innerHTML = `<p>Move set not available for ${pokemonName} in Generation ${generation}.</p>`;
+    return;
+  } else if (pokemon.moves.length === 0) {
+    resultsDiv.innerHTML = `<p>No move data available for ${pokemonName}.</p>`;
+    return;
+  }
+*/
+
   const stats = pokemon.stats.map((stat) => {
     return `${stat.stat.name}: ${stat.base_stat}`;
   }).join('<br>');
@@ -37,7 +78,8 @@ form.addEventListener('submit', async (event) => {
     <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
     <p><strong>Type:</strong> ${types}</p>
     <p><strong>Abilities:</strong> ${abilities}</p>
-    <p><strong>Move Set:</strong> ${moveSet.join(', ')}</p>
+    <p><strong>Move Set for Generation ${generation}:</strong> ${moveSet.join(', ')}</p>
     <p><strong>Stats:</strong><br>${stats}</p>
   `;
+
 });
