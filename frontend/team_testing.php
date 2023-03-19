ee24e84c34c62cde48879ffc27175b31398a5129
JacklinWey <jacklinwey@gmail.com>
2023-03-19 19:08:41 -0400

    team testing base output

--- frontend/team_testing.php
+++ frontend/team_testing.php
@@ -8,6 +8,8 @@
 </head>
 <body>
 <h1>Team Page</h1>
+
+<!--
 <br>
 
 	<form method="get" id="addTeam">
@@ -15,7 +17,7 @@
 		<button type="submit" name="team">Team</button>
 
 	</form>
-	<?php
+	<?php /*
 	if (isset($_GET['team'])) {
 		// User clicked the "Team" button
 		echo "You clicked the Team button!";
@@ -23,6 +25,7 @@
 		// User did not click the "Team" button
 		echo "Please click the Team button.";
 	}
+	*/
 	?>
 
 <div>
@@ -31,40 +34,28 @@
 	<label for="teamSelection">Select a Team:</label>
 	 <select id="teamSelection">
          <option value="0">New Team</option>
-   
-<?php
-    
-	require_once('path.inc');
-	require_once('get_host_info.inc');
-	require_once('rabbitMQLib.inc');	
-	$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-	session_start();
-	$request = array();
-	$request['type'] = "getteaminfo";
-	$request['UserID'] = $_SESSION['UserID'];
-	$request['message'] = "hi";
-  
-	$response = $client->send_request($request);	
+   !-->
+   <?php
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');	
+$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+session_start();
+$request = array();
+$request['type'] = "getteaminfo";
+$request['UserID'] = $_SESSION['UserID'];
+$request['TeamID'] = $object['TeamID'];
+$request['message'] = "hi";
+$response = $client->send_request($request);	
 	if (true) 
 	{			        
 	    $arry = json_decode($response['message'], true);
-	    foreach($arry as $row)
-	    {
-		 //   echo "<option value='1'>New Opt</option>";
-
-	      echo "<option value='{$row['TeamID']}'>{$row['TeamName']}</option>";
-	    }	  
-	}
-	else {
-	echo "<option value='1'>{$response['returncode']} Opt</option>";
-	}
-
-	?>
-	</select>
-	 <button id="addTeam" class="addTeam" ></button>
-	 <input id ="teamName" type="text" value = "New Pokemon Team">
-     </form>
-     
-</div>
-</body>
-</html>
+      echo "<table><tr><th>Team ID</th><th>Team</th></tr>";
+      foreach ($arry as $row){
+        echo "<tr><td>" . $row['TeamID'] . "</td><td>" . $row['TeamName'];
+         echo "</table>";
+      }
+      }
+      else{
+        echo "No Results";
+    }
