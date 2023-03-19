2baede2affe89712bc0107e9b5a7274541137b4e
branit490 <bs585@njit.edu>
2023-03-19 23:47:44 +0000

    buttons are now being displayed

--- frontend/team_testing.php
+++ frontend/team_testing.php
@@ -1,65 +1,52 @@
 <!DOCTYPE html>
 <html>
 <?php require(__DIR__ . "/nav.php"); ?>
-<?php     session_start(); ?> 
+<?php session_start(); ?>
 
 <head>
 	<title>Teams Page</title>
 </head>
+
 <body>
-<h1>Team Page</h1>
+	<h1>Team Page</h1>
 
-<!--
-<br>
+	<div>
+		<form method="get" id="addTeam">
+			<!--<button type="submit" name="team" value="new">Butt</button> -->
+			<?php
+			require_once('path.inc');
+			require_once('get_host_info.inc');
+			require_once('rabbitMQLib.inc');
+			$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+			$request = array();
+			$request['type'] = "getteaminfo";
+			$request['UserID'] = $_SESSION['UserID'];
+			$request['message'] = "hi";
+			$response = $client->send_request($request);
+			if (isset($response['message'])) {
+				$teams = json_decode($response['message'], true);
+				if (count($teams) > 0) {
+					foreach ($teams as $team) {
+						echo '<button type="submit" name="team" value="' . $team['TeamID'] . '">' . $team['TeamName'] . '</button>';
+					}
+				}
+			} else {
+				echo "<p>No teams found.</p>";
+			}
+			?>
+		</form>
+	</div>
 
-	<form method="get" id="addTeam">
-    
-		<button type="submit" name="team">Team</button>
-
-	</form>
-	<?php /*
+	<?php
 	if (isset($_GET['team'])) {
-		// User clicked the "Team" button
-		echo "You clicked the Team button!";
-	} else {
-		// User did not click the "Team" button
-		echo "Please click the Team button.";
+		$selectedTeam = $_GET['team'];
+		if ($selectedTeam == "new") {
+			// create new team
+		} else {
+			// display team information
+		}
 	}
-	*/
 	?>
-
-<div>
-    <div>
-     <form id="addTeamForm">     
-	<label for="teamSelection">Select a Team:</label>
-	 <select id="teamSelection">
-         <option value="0">New Team</option>
-   !-->
-   <?php
-require_once('path.inc');
-require_once('get_host_info.inc');
-require_once('rabbitMQLib.inc');	
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
-session_start();
-$request = array();
-$request['type'] = "getteaminfo";
-$request['UserID'] = $_SESSION['UserID'];
-$request['TeamID'] = $object['TeamID'];
-$request['message'] = "hi";
-$response = $client->send_request($request);	
-	if (true) 
-	{			        
-	    $arry = json_decode($response['message'], true);
-      echo "<table><th>Team</th></tr>";
-      foreach ($arry as $row){
-        echo  "</td><br><td>" . $row['TeamName'];
-         echo "</table>";
-      }
-      }
-      else{
-        echo "No Results";
-    }
-    ?>
 </body>
 
 </html>
