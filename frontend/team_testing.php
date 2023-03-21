8cf7a54004796de8d8fa4b267742994039fbf2b2
BenBandila <benbandila11@gmail.com>
2023-03-20 21:51:56 -0400

    hopeful changes to teams page

--- frontend/team_testing.php
+++ frontend/team_testing.php
@@ -11,53 +11,66 @@
 	<h1>Team Page</h1>
 
 	<div>
-		<form method="get" id="addTeam">
-			<!--<button type="submit" name="team" value="new">Butt</button> -->
-			<?php
-			require_once('path.inc');
-			require_once('get_host_info.inc');
-			require_once('rabbitMQLib.inc');
-			$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-			$request = array();
-			$request['type'] = "getteaminfo";
-			$request['UserID'] = $_SESSION['UserID'];
-			$request['message'] = "hi";
-			$response = $client->send_request($request);
-			if (isset($response['message'])) {
-				$teams = json_decode($response['message'], true);
-				if (count($teams) > 0) {
-					foreach ($teams as $team) {
-						echo '<button type="submit" name="team" value="' . $team['TeamID'] . '">' . $team['TeamName'] . '</button>';
+		<form method="get">
+			<select id="team" name="team">
+				<!--<button type="submit" name="team" value="new">Butt</button> -->
+				<?php
+				//include('messageManager.php');
+				require_once('path.inc');
+      			require_once('get_host_info.inc');
+	      		require_once('rabbitMQLib.inc');
+				$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+				$request = array();
+				$request['type'] = "getteaminfo";
+				$request['UserID'] = $_SESSION['UserID'];
+				$request['message'] = "hi";
+				$response = $client->send_request($request);
+				//$response = directMessage($request, 'testServer');
+				if (isset($response['message'])) {
+					$teams = json_decode($response['message'], true);
+					foreach($teams as $team1) {
+						echo '<option value="' . $team1['TeamID'] . '">' . $team1['TeamName'] . '</option>';
 					}
+					if (count($teams) > 0) {
+						foreach ($teams as $team) {
+							echo "<div>";
+							$client1 = new rabbitMQClient("testRabbitMQ.ini","testServer");
+							$request = array();
+							$request['type'] = "getteam";
+							$request['TeamID'] = $team['TeamID'];
+							$request['UserID'] = $_SESSION['UserID'];
+							$getTeam = $client1->send_request($request);
+							//$getTeam = directMessage($request, 'testServer');
+							$pokemons = json_decode($getTeam['message']);
+							foreach($pokemons as $pokemon) {
+								echo "<p>".$pokemon['PokemonName']."</p>";
+							}
+							echo "</div>";
+						}
+					}
+				} else {
+					echo "<p>No teams found.</p>";
 				}
-			} else {
-				echo "<p>No teams found.</p>";
-			}
-			?>
+				?>
+			</select>
+			<button type="submit">Show Team</button>
 		</form>
 	</div>
 
 	<?php
+	/*
 	if (isset($_GET['team'])) {
-
-
+		$selectedTeam = $_GET['team'];
         require_once('path.inc');
 		require_once('get_host_info.inc');
 		require_once('rabbitMQLib.inc');
 		$client1 = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-
 		$request = array();
-        
 		$request['type'] = "getteam";
-
-
         $request['UserID'] = $_SESSION['UserID'];
         $request['TeamID'] = $selectedTeam;
-		$selectedTeam = $_GET['team'];
 		//try to log request and the response to see what you're getting back
-
         $request['message'] = "hi";
-
         $response = $client1->send_request($request);
         if (isset($response['message'])) {
             $team = json_decode($response['message'], true);
@@ -67,10 +80,10 @@
         }
 
 	}
-        else{
-            echo '<p> Team not set! </p>';
-        }
-	
+    else{
+        echo '<p> Team not set! </p>';
+    }
+	*/
     ?>
     
 </body>
