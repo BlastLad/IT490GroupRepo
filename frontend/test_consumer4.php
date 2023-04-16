46bef54e970ef9a05e4480240a598ef87692bb97
branit490 <bs585@njit.edu>
2023-04-16 20:15:15 +0000

    adding these two files

---
+++ frontend/test_consumer4.php
@@ -0,0 +1,57 @@
+#!/usr/bin/php
+<?php
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$packageDir = '/home/branit490/workspace/packages';
+
+echo "Package Consumer START".PHP_EOL;
+
+function process_package($path, $destination_path)
+{
+    $filename = basename($path);
+
+    // check if package directory exists, create it if it doesn't
+    if (!file_exists($destination_path)) {
+        mkdir($destination_path, 0777, true);
+    }
+
+    // check if the package file already exists
+    if (file_exists("$destination_path/$filename")) {
+        echo "Package already exists".PHP_EOL;
+        return "Package already exists";
+    }
+
+    // move the package to the destination directory
+    if (copy($path, "$destination_path/$filename")) {
+        echo "Package saved".PHP_EOL;
+        return "Package saved";
+    } else {
+        echo "Failed to save package".PHP_EOL;
+        return "Failed to save package";
+    }
+}
+
+function request_processor($request)
+{
+    echo "Received Request".PHP_EOL;
+    var_dump($request);
+
+    if (!isset($request['type'])) {
+        return "ERROR: Unsupported message type";
+    }
+
+    switch ($request['type']) {
+        case 'package':
+            return process_package($request['package_path'], $packageDir);
+        default:
+            return "ERROR: Unsupported message type";
+    }
+}
+
+$server = new rabbitMQServer("testRabbitMQ.ini", "logger");
+$server->process_requests('request_processor');
+
+exit();
---
+++ frontend/test_package4.php
@@ -0,0 +1,65 @@
+#!/usr/bin/php
+<?php
+
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+
+$packageNumberFile = '/home/branit490/workspace/packages/last_package_number.txt';
+$zipCountFile = '/home/branit490/workspace/packages/zip_count.txt';
+$packageDir = '/home/branit490/workspace/packages';
+
+// get the last package number and zip count from the files
+$packageNumber = intval(file_get_contents($packageNumberFile));
+$zipCount = intval(file_get_contents($zipCountFile));
+
+echo "Package Producer START".PHP_EOL;
+
+do {
+    $input = readline("Ready to create a package? (y/n): ");
+} while ($input !== 'y' && $input !== 'n');
+
+if ($input === 'y') {
+    // create zip file with package number
+    $zipFilePath = "$packageDir/package$packageNumber.zip";
+    $cmd = "cd /home/branit490/workspace/IT490GroupRepo && zip -r $zipFilePath *";
+    shell_exec($cmd);
+
+    do {
+        $input = readline("Package created. Ready to send package? (y/n): ");
+    } while ($input !== 'y' && $input !== 'n');
+
+    if ($input === 'y') {
+        // send the package to the consumer
+        $remoteHost = '192.168.192.247';
+        $remotePort = '5672';
+        
+
+        $client = new rabbitMQClient("testRabbitMQ.ini", "logger");
+        $zipFileData = (file_get_contents($zipFilePath));
+        $request = array(
+            'type' => 'package',
+            'zipFile' => $zipFileData,
+            'destination_path' => '/home/benbandila/test_packages'
+        );
+        $response = $client->send_request($request);
+
+        // check if the package was saved successfully
+        if ($response == "Package saved") {
+            // increment package number and zip count
+            $packageNumber++;
+            $zipCount++;
+
+            // update the package number and zip count files
+            file_put_contents($packageNumberFile, $packageNumber);
+            file_put_contents($zipCountFile, $zipCount);
+
+            echo "Package saved as $zipFilePath".PHP_EOL;
+            echo "Total number of zip files created: $zipCount".PHP_EOL;
+        } else {
+            echo "Failed to save package".PHP_EOL;
+        }
+    }
+}
+
+exit();
