2d5207151f82629f500dba684721cfe0174362a1
BenBandila <benbandila11@gmail.com>
2023-04-18 19:08:17 -0400

    fixed copying to /var/www/sample issue

--- frontend/unzip.php
+++ frontend/unzip.php
@@ -7,10 +7,15 @@ $zipFilePath = '/home/benbandila/' . $fileName;
 $destination = '/home/benbandila/test_packages/' . $folderName;
 $cmd = "unzip $zipFilePath -d $destination";
 shell_exec($cmd);
-$cmd1 = "cd $destination && cd frontend/";
+$cmd1 = "cd $destination";
 shell_exec($cmd1);
-shell_exec("sh ben_copy.sh");
-shell_exec("sudo service apache2 restart");
+$string = "for file in ~/test_packages/$folderName/*;";
+$copyFile = "$destination/ben_copy.sh";
+$lines = file($copyFile);
+$lines[2] = $string . PHP_EOL;
+file_put_contents($copyFile, implode('', $lines));
+shell_exec("sh $copyFile");
+//shell_exec("sudo service apache2 restart");
 exit();
 ?>
 
